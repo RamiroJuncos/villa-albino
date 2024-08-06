ed5ad6689bf26a7388a5417e3417b14ef35adc8f
jhonviafara <jhon1144105@gmail.com>
2024-08-06 01:08:02 -0300

    Guardar cambios locales en Login.jsx

---
+++ src/pages/home.pages.jsx
@@ -0,0 +1,28 @@
+import Contacto from "../components/Contacto";
+import Header from "../components/Header";
+import Noticias from "../components/Noticias";
+import PieDePagina from "../components/PiePagina";
+import ProximosPartidos from "../components/ProximosPartidos";
+import TituloPrincipal from "../components/TituloPrincipal";
+import UltimosResultados from "../components/UltimosResultados";
+import Fondo from "../assets/Foto-Fondo.jpg"
+
+function Home() {
+    return (
+        <div>
+             <div className=""
+         style={{backgroundImage: `url(${Fondo})`}}>
+            <Header />
+            <TituloPrincipal/>
+            <UltimosResultados />
+            <ProximosPartidos />
+            <Noticias/>
+            <Contacto />
+            <PieDePagina />
+        </div>
+        </div>
+    );
+}
+
+export default Home;
+
