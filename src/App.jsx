b15b58b77fd9201f4950e8ccde8bbecb907552ab
jhonviafara <jhon1144105@gmail.com>
2024-08-06 01:32:12 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
-<<<<<<< HEAD
 import Home from './pages/home.pages'
 import Logue from "./pages/login.form";
-=======
-import Login from "./pages/login.pages";
-
-
-
-
-
-
-
-
->>>>>>> cbe5d73e7bf5e7a70c90d2f42837096d468884ca
-
 function App() {
+  return (
 <BrowserRouter>
 <Routes>
   <Route path="/" element={<Logue/>}/>
   <Route path="" element= {<Home/> }/>
 </Routes>
-
 </BrowserRouter>
-
-<<<<<<< HEAD
-=======
-  return (
-
-    <>
-      <Login/>
-    </>
   )
->>>>>>> cbe5d73e7bf5e7a70c90d2f42837096d468884ca
 }
 
 export default App
