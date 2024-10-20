dc3811893608546c767da5ffd6820cf5146a65d9
jhonviafara <jhon1144105@gmail.com>
2024-10-20 00:56:24 -0300

    arreglos en el header

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css';
-import Home from './pages/home.pages';
-import PlayersTable from "./pages/planilla.pages";
-import Login from "./pages/login.pages";
-
+import './App.css'
+import Home from './pages/home.pages'
+import Logue from "./pages/login.pages";
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="planilla" element={<PlayersTable />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Logue />}/>
+      <Route path="/home" element= {<Home /> }/>
+    </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App;
